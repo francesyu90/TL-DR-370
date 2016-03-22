2ede5cefcc47141ab085dd354a024ca941dbc49c
francesyu90 <francesyu90@yahoo.com>
2016-03-21 18:04:10 -0700

    Upload sample codes & source file for apache-tomcat

---
+++ CSC370ProjectSample/development/MovieServlet1.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class MovieServlet1 extends HttpServlet {
+    void processRequest(HttpServletRequest request, HttpServletResponse response)
+    										throws ServletException, IOException {
+      response.setContentType("text/html;charset=UTF-8");
+      PrintWriter out = response.getWriter();
+        
+	  Connection conn = ConnectionManager.getInstance().getConnection();
+      try { 
+    	  Statement stmt = conn.createStatement();
+    	  ResultSet rset = stmt.executeQuery(
+                        "SELECT title, year " +
+                        "FROM Movies");
+      
+    	  out.println("<table>");
+          while (rset.next()) {
+        	  out.println(
+        	  "<tr>" +
+               	  "<td>"+rset.getString("title")+"</td>" + 
+               	  "<td><A href=\"http://localhost:8081/servlet/MovieServlet2?year="+		
+               		 rset.getString("year")+"\">"+rset.getString("year")+"</A>"+"</td>"+
+              "</tr>");
+          }
+          out.println("</table>");
+          stmt.close();
+      }
+      catch(SQLException e) { out.println(e); }
+      ConnectionManager.getInstance().returnConnection(conn);
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                   						throws ServletException, IOException { 
+    	processRequest(request, response);     }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                   						throws ServletException, IOException {
+    	processRequest(request, response);     }
+    
+    public String getServletInfo() {  return "Movie Servlet 1"; }
+}
