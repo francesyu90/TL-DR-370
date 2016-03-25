33af41b47720244d4e018f32fe39578717ee744a
francesyu90 <francesyu90@yahoo.com>
2016-03-25 15:37:04 -0700

    Insert Route successfully

--- development/InsertRoutes.java
+++ development/InsertRoutes.java
@@ -3,7 +3,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
 
-public class InsertAirlines extends HttpServlet {
+public class InsertRoutes extends HttpServlet {
      void processRequest(HttpServletRequest request, HttpServletResponse response) 
                             throws ServletException, IOException {
         
@@ -12,17 +12,37 @@ public class InsertAirlines extends HttpServlet {
         PrintWriter out = response.getWriter();
         
         String acode = request.getParameter("acode");
-        String aname = request.getParameter("aname");
-        String awebsite = request.getParameter("awebsite");
+        String rnum = request.getParameter("rnum");
+        String plmodel = request.getParameter("plmodel");
 
-        String statementString = 
-        "INSERT INTO Airlines(acode, name, website) " +
-        "VALUES( '" + acode + "','" + aname + "','" + awebsite + "')";
+         String statementString = 
+        "INSERT INTO Routes(rnum, plane_model, acode) " +
+        "VALUES( " + rnum + ",'" + plmodel + "','" + acode + "')";
+
+        String test = request.getParameter("checkRoute");
+        String place = request.getParameter("place");
+        String time = request.getParameter("time");
+
+       
+
+        //mm:ss
+
+
+        String statementStringI = null;
+
+        if (test.equals("in")) {
+            statementStringI =  "INSERT INTO Incoming_Routes(rnum, acode, source, in_T) " +
+        "VALUES( " + rnum + ",'" + acode + "','" + place + "', TO_DATE('"+ time +"', 'hh24:mi'))";
+        }else{
+             statementStringI =  "INSERT INTO Outgoing_Routes(rnum, acode, destination, out_T) " +
+        "VALUES( " + rnum + ",'" + acode + "','" + place + "', TO_DATE('"+ time +"', 'hh24:mi'))";
+        }
 
        Connection conn = ConnectionManager.getInstance().getConnection();
         try {
             Statement stmt = conn.createStatement();
             stmt.executeUpdate(statementString);
+            stmt.executeUpdate(statementStringI);
             stmt.close();
             out.println("Insertion Successful!");
         }
